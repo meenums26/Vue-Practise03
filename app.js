986b97814f237cc885aefd2c2280f74a180d504a
MS <meenu.ms@capgemini.com>
2024-10-07 23:09:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+Vue.createApp({
+ data (){
+    return {
+    name : "Meenu M S",
+    age : 29,
+    imgLink : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzk92qOx7c5k5fybjVbUkwg6BGW_ptjgID9A&s"
+ }
+},
+ methods:{
+    ageInFiveYrs (){
+        return this.age + 5;
+    },
+    favouriteNum (){
+      return Math.random();
+    }
+ }
+}).mount("#assignment");
