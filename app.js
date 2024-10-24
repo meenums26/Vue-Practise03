8468a8c34113d988e31daebd4651d735d6b90757
MS <meenu.ms@capgemini.com>
2024-10-24 16:01:11 +0530

    computed and watchers

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ Vue.createApp({
     name : "",
     age : 29,
     imgLink : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzk92qOx7c5k5fybjVbUkwg6BGW_ptjgID9A&s",
-    counter:0
+    counter:0,
+    fullname:""
  }
 },
  methods:{
@@ -22,6 +23,14 @@ Vue.createApp({
             this.counter = this.counter-num;
         }
     },
+    outputFullName(){
+        if(this.name === ''){
+            return ''
+        }else{
+            return this.name + ' ' + 'Soman'
+        }
+        
+    },
     setName(e){
         this.name = e.target.value;
     },
@@ -31,5 +40,23 @@ Vue.createApp({
     resetInput(){
         this.name = ''
     }
- }
+ },
+ watch:{
+    name(value){
+        if( value === '' ){
+           this.fullname = '';
+        }else{
+            this.fullname = value + ' ' + 'Soman'
+        }
+    }
+},
+computed : {
+    // fullName(){
+    //    if(this.name === ''){
+    //     return ''
+    // }else{
+    //     return this.name + ' ' + 'Soman'
+    // } 
+    // }
+},
 }).mount("#assignment");
